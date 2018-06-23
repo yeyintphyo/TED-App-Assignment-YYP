95b9847b16d2168ee7aa10a94e655287ce0a284d
Unknown <yeyint.china@gmail.com>
2018-06-23 11:26:36 +0630

    add OkHttpDataAgent and RetrofitDataAgent for network layer

---
+++ app/src/main/java/com/padcmyanmar/ted_app_assignment_yyp/network/RetrofitTalksDataAgentImpl.java
@@ -0,0 +1,76 @@
+package com.padcmyanmar.ted_app_assignment_yyp.network;
+
+import com.padcmyanmar.ted_app_assignment_yyp.events.ApiErrorEvent;
+import com.padcmyanmar.ted_app_assignment_yyp.events.SuccessGetTalksEvent;
+import com.padcmyanmar.ted_app_assignment_yyp.network.response.GetTalksResponse;
+import com.padcmyanmar.ted_app_assignment_yyp.utils.TalksConstants;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitTalksDataAgent implements TalksDataAgent {
+
+    private static RetrofitTalksDataAgent sObjInstance;
+
+    private TalksApi mTheApi;
+
+    private RetrofitTalksDataAgent() {
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .writeTimeout(15, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(TalksConstants.API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+
+        mTheApi = retrofit.create(TalksApi.class);
+    }
+
+    public static RetrofitTalksDataAgent getInstance() {
+        if (sObjInstance == null) {
+            sObjInstance = new RetrofitTalksDataAgent();
+        }
+        return sObjInstance;
+    }
+
+    @Override
+    public void loadTalksList(int page, String accessToken) {
+        Call<GetTalksResponse> loadNewsCall =  mTheApi.loadNewsList(accessToken, page);
+        loadNewsCall.enqueue(new Callback<GetTalksResponse>() {
+            @Override
+            public void onResponse(Call<GetTalksResponse> call, Response<GetTalksResponse> response) {
+                GetTalksResponse newsResponse = response.body();
+                if (newsResponse != null && newsResponse.isResponseOK()) {
+                    SuccessGetTalksEvent event = new SuccessGetTalksEvent(newsResponse.getTedTalks());
+                    EventBus.getDefault().post(event);
+                } else {
+                    if (newsResponse == null) {
+                        ApiErrorEvent event = new ApiErrorEvent("Empty response in network call.");
+                        EventBus.getDefault().post(event);
+                    } else {
+                        ApiErrorEvent event = new ApiErrorEvent(newsResponse.getMessage());
+                        EventBus.getDefault().post(event);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GetTalksResponse> call, Throwable t) {
+                ApiErrorEvent event = new ApiErrorEvent(t.getMessage());
+                EventBus.getDefault().post(event);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/padcmyanmar/ted_app_assignment_yyp/network/TalksApi.java
@@ -0,0 +1,4 @@
+package com.padcmyanmar.ted_app_assignment_yyp.network;
+
+public interface TalksApi {
+}
